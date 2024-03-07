1ac07e2267cbb933f12c251ed508c1ee3a94b837
estefania <estefania.loaizas@udea.edu.co>
2024-03-07 16:53:06 -0500

    v2: creo el init de la clase implantes medicos que es la clase padre de la cual los demas implantes van a heredar sus metodos

--- EstefaniaLoaizaG1_ MarianaVasquezG2.py
+++ EstefaniaLoaizaG1_ MarianaVasquezG2.py
@@ -1,6 +1,13 @@
+#Se crea la clase padre implantes_M que es la de las caracteristicas generales de los implantes medicos
 class Implantes_M():
-    pass
-
+    #Se  inicializa la clase con el metodo init
+    def __init__(self):
+        self.__nombre = ""
+        self.__id = 0
+        self.__fecha = ""
+        self.__fabricante = ""
+        self.__estado = ""
+         
 class Protesis_C(Implantes_M):
     pass
 
