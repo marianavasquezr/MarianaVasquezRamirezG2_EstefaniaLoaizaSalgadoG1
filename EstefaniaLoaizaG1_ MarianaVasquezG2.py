13577826b684b2d99f6c7f058088fc99315b1786
Mariana Vasquez <mariana.vasquezr@udea.edu.co>
2024-03-07 16:35:19 -0500

    Version 1: Se crea el cuerpo con las clases incialmente necesarias

---
+++ EstefaniaLoaizaG1_ MarianaVasquezG2.py
@@ -0,0 +1,20 @@
+class Implantes_M():
+    pass
+
+class Protesis_C(Implantes_M):
+    pass
+
+class Marcapasos(Implantes_M):
+    pass
+
+class Stents(Implantes_M):
+    pass
+
+class ImplantesD(Implantes_M):
+    pass
+
+class Protesis_R(Implantes_M):
+    pass
+
+class Sistema():
+    pass
