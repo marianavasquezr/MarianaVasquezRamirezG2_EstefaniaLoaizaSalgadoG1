1b12bc95be93a3bcd89d0e68bbac847973b6960f
estefania <estefania.loaizas@udea.edu.co>
2024-03-07 16:58:35 -0500

    v3: hacemos los getters y setters de la clase padre

--- EstefaniaLoaizaG1_ MarianaVasquezG2.py
+++ EstefaniaLoaizaG1_ MarianaVasquezG2.py
@@ -7,7 +7,39 @@ class Implantes_M():
         self.__fecha = ""
         self.__fabricante = ""
         self.__estado = ""
-         
+     
+    #setters
+    def asignarNombre(self, n):
+        self.__nombre = n
+        
+    def asignarId(self, id):
+        self.__id = id
+        
+    def asignarFecha(self, f):
+        self.__fecha = f
+        
+    def asignarFabricante(self, fa):
+        self.__fabricante = fa
+        
+    def asignarEstado(self, e):
+        self.__estado = e
+        
+    #getters
+    def verNombre(self):
+        return self.__nombre
+    
+    def verId(self):
+        return self.__id
+    
+    def verFecha(self):
+        return self.__fecha
+    
+    def verFabricante(self):
+        return self.__fabricante
+    
+    def verEstado(self):
+        return self.__estado
+        
 class Protesis_C(Implantes_M):
     pass
 
